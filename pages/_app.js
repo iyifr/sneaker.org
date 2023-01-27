ff60d5e0bced1b3d267adf01ace50083240d70dd
Iyimide Adekile <105992164+iyifr@users.noreply.github.com>
2023-01-27 22:12:05 +0100

    Le navbares

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import "@fontsource/kumbh-sans/400.css"
+import "@fontsource/kumbh-sans/700.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
